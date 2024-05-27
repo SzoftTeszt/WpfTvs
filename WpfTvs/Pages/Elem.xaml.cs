356c1cc92135de1ccc0474da3bcbaeca299af1e9
Jáger Attila <jagerattila@gmail.com>
2024-05-27 13:48:30 +0200

    Add project files.

---
+++ WpfTvs/Pages/Elem.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfTvs.Pages
+{
+    /// <summary>
+    /// Interaction logic for Elem.xaml
+    /// </summary>
+    public partial class Elem : UserControl
+    {
+        public Elem()
+        {
+            InitializeComponent();
+        }
+    }
+}
